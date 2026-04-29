03ffae4c92e6255bff5875d72c9de7e7f47f0c99
Mike Aranda <mikedev64>
2026-04-28 22:02:05 -0700

    solved issue

--- src/db_connection/supabase.ts
+++ src/db_connection/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
-const supabaseurl = "https://kblypeoouzuqazzchtfg.supabase.co"
-const api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtibHlwZW9vdXp1cWF6emNodGZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0MDQ0MDAsImV4cCI6MjA4Nzk4MDQwMH0.Rrn6H_H8abqW8R_dAzpQcTCJ-M12g9vRKscB4Uuh6jI"
+const supabaseurl = ""
+const api_key = ""
 
 export const supabase = createClient(supabaseurl, api_key, {
   auth: {
